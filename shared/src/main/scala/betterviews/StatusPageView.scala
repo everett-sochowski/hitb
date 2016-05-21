c05713ec929c2120054cbe49e7cb8ce18d4626e4
Clemens Winter <clemenswinter1@gmail.com>
2016-05-21 18:48:56 +0200

    Improve status page layout

--- shared/src/main/scala/betterviews/StatusPageView.scala
+++ shared/src/main/scala/betterviews/StatusPageView.scala
@@ -8,37 +8,45 @@ import scalatags.Text.all._
 object StatusPageView {
   def static(status: JobsStatus): String = Main("Status")(content(status))
 
-  def dynamic() = Main("Status")(onload:="scripts.StatusPage().run()")
+  def dynamic() = Main("Status")(onload := "scripts.StatusPage().run()")
 
   def content(status: JobsStatus) = div(
-    h1("Status"),
-    h2("Overview"),
-    "Unassigned work items: ", status.workItems, br,
-    "Pending jobs: ", status.pendingJobs, br,
-    "Failed jobs: ", status.failedJobs, br,
-    "Completed jobs: ", status.results.size,
-    h2("Aggregate Jobs"),
-    p(
-      ul(
-        for (AggregateJobStatus(id, completed, pending) <- status.pendingAggregateJobs)
-          yield li(s"Aggregate Job ${id.value}: [$completed/${completed + pending}]")
-      )
-    ),
-    h2("Recent Results"),
-    p(
-      ul(
-        for (result <- status.results.takeRight(10))
-          yield resultLi(result)
+    h1(textAlign:="center", "Status"),
+    div(
+      display:="flex",
+      div(
+        flexGrow:="1",
+        h2("Overview"),
+        "Unassigned work items: ", status.workItems, br,
+        "Pending jobs: ", status.pendingJobs, br,
+        "Failed jobs: ", status.failedJobs, br,
+        "Completed jobs: ", status.results.size
+      ),
+      div(
+        flexGrow:="1",
+        h2("Aggregate Jobs"),
+        p(
+          for (AggregateJobStatus(id, completed, pending) <- status.pendingAggregateJobs)
+            yield Seq[Modifier](s"Aggregate Job ${id.value}: [$completed/${completed + pending}]", br)
+        )
+      ),
+      div(
+        flexGrow:="1",
+        h2("Recent Results"),
+        p(
+          for (result <- status.results.takeRight(20).reverse)
+            yield Seq[Modifier](resultItem(result), br)
+        )
       )
     )
   )
 
-  def resultLi(r: Result) = r match {
+  def resultItem(r: Result) = r match {
     case DoubleResult(id, value) =>
-      li(s"Job ${id.value}: ${value}")
+      s"Job ${id.value}: ${value}"
     case OptionalDoubleResult(id, value) =>
       val formattedValue = value.fold("No result found")(_.toString)
-      li(s"Job ${id.value}: $formattedValue")
+      s"Job ${id.value}: $formattedValue"
   }
 }
 
