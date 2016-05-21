dc847717f5495fc95dd88618e81d225f8e0025a7
Clemens Winter <clemenswinter1@gmail.com>
2016-05-21 16:58:12 +0200

    Fix regression

--- clientworker/src/main/scala/hitb/Worker.scala
+++ clientworker/src/main/scala/hitb/Worker.scala
@@ -36,6 +36,7 @@ object Worker extends js.JSApp {
   }
 
   private def postResult[T: Writer](result: T) = {
+    self.postMessage(result.asInstanceOf[scala.scalajs.js.Any])
     Ajax.post("/postResult", write(result))
   }
 }
